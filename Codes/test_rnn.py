18563677b81c8d870458eb77a22f529ca823bf23
Leonardo <rlsandovalp@unal.edu.co>
2022-04-19 17:07:07 +0200

    modify train

--- Codes/test_rnn.py
+++ Codes/test_rnn.py
@@ -5,7 +5,7 @@ from functions_ML import *
 
 
 window = 8
-anticipation = 2
+anticipation = 1
 
 training_variables = [['Costa', '8148_1'],
                     ['Molteno', '9084_1'],
@@ -18,11 +18,11 @@ training_variables = [['Costa', '8148_1'],
 
 target_variable = ['Costa', '8148_1']
 
-model = model = tf.keras.models.load_model('rnn_model_wf_ant2.h5')
+model = model = tf.keras.models.load_model('rnn_model_wf_ant1.h5')
 
 starting_point = 0
-training_size = 0
-testing_size = 20
+training_size = 57000
+testing_size = 1000
 
 X_test = np.zeros((testing_size, len(training_variables)))
 Y_test = np.zeros(testing_size)
@@ -35,9 +35,9 @@ Y_test = pd.read_csv('../joined_data/'+target_variable[0]+'/'+target_variable[1]
 X = []
 Y = []
 
-for i in range (window+anticipation+1,testing_size):
-    X.append(X_test[i-window-anticipation-1:i-anticipation-1,:])
-    Y.append(Y_test[i])
+for i in range (window+anticipation,testing_size):
+    X.append(X_test[i-window-anticipation:i-anticipation,:])
+    Y.append(Y_test[i-1])
 
 
 ## TUTTO E UN CASINO!!
@@ -55,35 +55,50 @@ plt.show()
 
 
 plt.figure(figsize = (10,6))
-plt.subplot(2,3,1)
+plt.subplot(3,3,1)
 plt.plot(Yp, 'r-', label = 'LSTM')
 plt.plot(Y_test, 'b-', label = 'Measured')
 plt.ylabel('Flowrate [cms]')
 plt.legend()
 plt.text(0.03, 10, 'MAPE = ' + str('%.2f' % mape(Yp, Y_test)) + '%')
 plt.text(0.03, 5, 'NSE = ' + str('%.2f' % nse(Yp, Y_test)))
-plt.subplot(2,3,2)
+plt.subplot(3,3,2)
 plt.plot(X_test[:,-1,0], label = 'Precipitation [mm]')
 plt.xlabel('Time [hours]')
-plt.ylabel('Precipitation [mm]')
+plt.ylabel('q [mm]')
 plt.legend()
-plt.subplot(2,3,3)
+plt.subplot(3,3,3)
 plt.plot(X_test[:,-1,1], label = 'Precipitation [mm]')
 plt.xlabel('Time [hours]')
-plt.ylabel('Precipitation [mm]')
+plt.ylabel('q [mm]')
 plt.legend()
-plt.subplot(2,3,4)
+plt.subplot(3,3,4)
 plt.plot(X_test[:,-1,2], label = 'Precipitation [mm]')
 plt.xlabel('Time [hours]')
-plt.ylabel('Precipitation [mm]')
+plt.ylabel('q [mm]')
 plt.legend()
-plt.subplot(2,3,5)
+plt.subplot(3,3,5)
 plt.plot(X_test[:,-1,3], label = 'Precipitation [mm]')
 plt.xlabel('Time [hours]')
 plt.ylabel('Precipitation [mm]')
 plt.legend()
-plt.subplot(2,3,6)
+plt.subplot(3,3,6)
 plt.plot(X_test[:,-1,4], label = 'Precipitation [mm]')
+plt.xlabel('Time [hours]')
+plt.ylabel('Precipitation [mm]')
+plt.legend()
+plt.subplot(3,3,7)
+plt.plot(X_test[:,-1,5], label = 'Precipitation [mm]')
+plt.xlabel('Time [hours]')
+plt.ylabel('Precipitation [mm]')
+plt.legend()
+plt.subplot(3,3,8)
+plt.plot(X_test[:,-1,6], label = 'Precipitation [mm]')
+plt.xlabel('Time [hours]')
+plt.ylabel('Precipitation [mm]')
+plt.legend()
+plt.subplot(3,3,9)
+plt.plot(X_test[:,-1,7], label = 'Precipitation [mm]')
 plt.xlabel('Time [hours]')
 plt.ylabel('Precipitation [mm]')
 plt.legend()
--- Codes/train_rnn.py
+++ Codes/train_rnn.py
@@ -23,15 +23,15 @@ target_variable = ['Costa', '8148_1']
             # DEFINE THE MODEL
 ########################################
 
-window = 8
-anticipation = 2
+window = 24
+anticipation = 1
 
 def make_model():
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.LSTM(50, return_sequences = True, input_shape = (window, len(training_variables))))
+    model.add(tf.keras.layers.LSTM(50, input_shape = (window, len(training_variables))))
     model.add(tf.keras.layers.Dropout(0.2))
-    model.add(tf.keras.layers.LSTM(50))
-    model.add(tf.keras.layers.Dropout(0.2))
+    # model.add(tf.keras.layers.LSTM(30))
+    # model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(1))
     return model
 
@@ -53,9 +53,9 @@ Y_train = pd.read_csv('../joined_data/'+target_variable[0]+'/'+target_variable[1
 X = []
 Y = []
 
-for i in range (window+anticipation-1,training_size):
-    X.append(X_train[i-window-anticipation+1:i-anticipation+1,:])
-    Y.append(Y_train[i])
+for i in range (window+anticipation,training_size):
+    X.append(X_train[i-window-anticipation:i-anticipation,:])
+    Y.append(Y_train[i-1])
 
 X_train, Y_train = np.array(X), np.array(Y)
 
@@ -79,7 +79,7 @@ callback = tf.keras.callbacks.EarlyStopping(monitor = 'loss', mode = 'min', verb
             # FIT THE MODEL
 ########################################
 t0 = time.time()
-history = model.fit(X_train, Y_train, batch_size = 1000, epochs = 100, callbacks = [callback])
+history = model.fit(X_train, Y_train, batch_size = 1000, epochs = 200, callbacks = [callback])
 t1 = time.time()
 print('Runtime: %.2f s' % (t1-t0))
 
@@ -89,4 +89,4 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.show()
 
-model.save('rnn_model_wf_ant2.h5')
+model.save('rnn_model_wf_ant1.h5')
